5bac4a636bdff65c01f24ab030c1676639fd245a
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-01-19 22:42:21 -0800

    Create NumbersDisappearedArray_LC_448.java

---
+++ NumbersDisappearedArray_LC_448.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> missingNumbers = new ArrayList<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i: nums){
+            set.add(i);
+        }
+        for(int i=1; i<=nums.length; i++){
+            if(!set.contains(i)){
+                missingNumbers.add(i);
+            }
+        }
+        return missingNumbers;
+    }
+}
