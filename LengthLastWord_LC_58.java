7303ff30c5373371259892838acf903a42830f96
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-01-20 17:27:37 -0800

    Create LengthLastWord_LC_58.java

---
+++ LengthLastWord_LC_58.java
@@ -0,0 +1,16 @@
+/* Author: Nandith Malapati*/
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int count = 0;
+        for(int i = str.length()-1 ; i>=0; i--){
+            if(str.charAt(i) != ' '){
+                count++;
+            }else{
+                break;
+            }
+        }
+        return count;
+    }
+}
