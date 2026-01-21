dabe22ed0d4de2ee2c2c607e5d10821c80210199
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-01-20 16:57:18 -0800

    Create RemoveDupFromSortedArr_LC_80.java

---
+++ RemoveDupFromSortedArr_LC_80.java
@@ -0,0 +1,18 @@
+/* Author: Nandith Reddy Malapati */
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int n = nums.length;
+        if(n <= 2) return n;
+        int i = 2; // slow pointer from index 2
+
+        for (int j = 2; j < n; j++){
+            if (nums[j] != nums[i-2] ){
+            nums[i] = nums[j];
+            i++;
+            }
+        }
+        return i;
+    }
+}
