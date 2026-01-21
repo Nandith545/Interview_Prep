6140deaf83727bc9b911ced39be4b2c91b75fd66
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-01-20 16:44:26 -0800

    Create MedianTwoSortedArrays_LC_04.java

---
+++ MedianTwoSortedArrays_LC_04.java
@@ -0,0 +1,39 @@
+/* Author: Nandith Malapati */
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    
+        int m = nums1.length;
+        int n = nums2.length;
+        int[] merged = new int[m+n];
+
+        int i=0, j=0, k=0;
+
+        //merge two sorted arrays
+        while(i<m && j<n){
+            if(nums1[i] <= nums2[j]){
+                merged[k++] = nums1[i++];
+            }else{
+                merged[k++] = nums2[j++];
+            }
+        }
+        //copy remaining elements
+        while(i<m){
+            merged[k++] = nums1[i++];
+        }
+        while(j<n){
+            merged[k++] = nums2[j++];
+        }
+
+        //find median
+        int total = m + n;
+        if(total % 2 == 0){
+            int mid1 = merged[total/2 - 1];
+            int mid2 = merged[total/2];
+            return ( (double) mid1 + mid2)/2;
+        }
+        else{
+            return merged[total/2];
+        }
+    }
+}
