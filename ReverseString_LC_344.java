35d231a6c3b8847d0de6ee88f15423e021a557a1
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-01-25 21:55:31 -0800

    Create ReverseString_LC_344.java

---
+++ ReverseString_LC_344.java
@@ -0,0 +1,13 @@
+/*Author: Nandith Malapati
+LC: Reverse a String*/
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left=0, right=s.length-1;
+        while(left< right){
+            char temp = s[left];
+            s[left++]=s[right];
+            s[right--] = temp;
+        }   
+    }
+}
