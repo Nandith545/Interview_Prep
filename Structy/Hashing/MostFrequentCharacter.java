e56a9bcb290b131feb2a7abf57ebcf4a893f02d8
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-02-20 22:53:42 -0800

    program to find most frequent character in a string

---
+++ Structy/Hashing/MostFrequentCharacter.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+class Source{
+    public static HashMap<Character, Integer> charCount(String s){
+        HashMap<Character, Integer> count = new HashMap<>();
+        for(char c : s.toCharArray()){
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+        return count;
+    }
+    public static char mostFrequentChar(String s){
+        HashMap<Character, Integer> count = charCount(s);
+        char mostFrequent = ' ';
+        for(char c: count.keySet()){
+            if(mostFrequent == ' ' ||count.get(c) > count.get(mostFrequent)){
+                mostFrequent = c;
+            }
+        }
+        return mostFrequent;
+    }
+}
+
+/*
+    n = length of string
+    time complexity O(n)
+    space complexity O(n)
+*/
