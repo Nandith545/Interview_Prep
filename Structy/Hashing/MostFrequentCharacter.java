6a2df4c876a5d128c2e039adc50811479bfa4bf0
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-02-20 22:58:21 -0800

    code update

--- Structy/Hashing/MostFrequentCharacter.java
+++ Structy/Hashing/MostFrequentCharacter.java
@@ -13,13 +13,18 @@ class Source{
     public static char mostFrequentChar(String s){
         HashMap<Character, Integer> count = charCount(s);
         char mostFrequent = ' ';
-        for(char c: count.keySet()){
+        for(char c: s.toCharArray()){
             if(mostFrequent == ' ' ||count.get(c) > count.get(mostFrequent)){
                 mostFrequent = c;
             }
         }
         return mostFrequent;
     }
+
+    public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
 }
 
 /*
