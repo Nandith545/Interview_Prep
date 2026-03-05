8c64f3bdcaa8f3d93c2f2c251385584a4a7d6606
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-03-04 23:20:57 -0800

    Recursive approach to remove a node in a linked List

---
+++ Structy/LinkedList/RemoveNode_Recursive.java
@@ -0,0 +1,25 @@
+class Node<T> {
+  T val;
+  Node<T> next;
+
+  public Node(T val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Source {
+  public static <T> Node<T> removeNode(Node<T> head, T targetVal) {
+    if (head.val == targetVal) {
+      return head.next;
+    }
+    
+    head.next = removeNode(head.next, targetVal);
+    return head;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
