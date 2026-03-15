9362a0c752283e8e7e34bbaf1d5f9259ab791071
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-03-14 22:47:05 -0700

    Tree Includes BFS

---
+++ Structy/BinaryTree/TreeIncludes_BFS.java
@@ -0,0 +1,41 @@
+import java.util.ArrayDeque;
+
+class Node<T> {
+  T val;
+  Node<T> left;
+  Node<T> right;
+  
+  public Node(T val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Source {
+  public static boolean treeIncludes(Node<String> root, String target) {
+    if (root == null) {
+      return false;
+    }
+    ArrayDeque<Node<String>> queue = new ArrayDeque<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      Node<String> current = queue.remove();
+      if (current.val == target) {
+        return true;
+      }
+      if (current.left != null) {
+        queue.add(current.left);
+      }
+      if (current.right != null) {
+        queue.add(current.right);
+      }
+    }
+    return false;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
