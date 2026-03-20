306d086623f2dacb165fcb7d220a49c45aaf2f1d
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-03-20 12:23:40 -0700

    Bottom Right Value in A Binary Tree

---
+++ Structy/BinaryTree/BottomRightValue_BFS.java
@@ -0,0 +1,38 @@
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+class Node<T> {
+  T val;
+  Node<T> left;
+  Node<T> right;
+  
+  public Node(T val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Source {
+  public static <T> T bottomRightValue(Node<T> root) {
+    Queue<Node<T>> queue = new ArrayDeque<>();
+    queue.add(root);
+    Node<T> node = null;
+    while(!queue.isEmpty()){
+      node = queue.remove();
+      T val = node.val;
+      if(node.left != null){
+        queue.add(node.left);
+      }
+      if(node.right != null){
+        queue.add(node.right);
+      }
+    }
+    return node.val;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
