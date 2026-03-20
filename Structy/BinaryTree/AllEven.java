29a69ac1325381f31c090294dfa2ee30f4ae84c0
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-03-20 12:41:47 -0700

    All Even in a list of Integers

---
+++ Structy/BinaryTree/AllEven.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+class Source {
+  public static boolean allEven(List<Integer> nums) {
+      for(int num: nums){
+        if(num%2 != 0){
+          return false;
+        }
+      }
+    return true;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function, but should not remove it
+  }
+}
