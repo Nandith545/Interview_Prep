39080336874315b1acd47bf8a1815f1192069cbf
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-03-19 22:56:43 -0700

    How high is tree recursion

---
+++ Structy/BinaryTree/Howhigh_Recursion.java
@@ -0,0 +1,25 @@
+class Node<T> {
+  T val;
+  Node<T> left;
+  Node<T> right;
+  
+  public Node(T val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Source {
+  public static <T> int howHigh(Node<T> root) {
+    if(root == null){
+      return -1;
+    }
+    return 1 + Math.max(howHigh(root.left) , howHigh(root.right));
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
