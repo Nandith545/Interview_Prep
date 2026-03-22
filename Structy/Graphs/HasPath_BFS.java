99c1ee58217ba830aebbcaed1d8907017264d54c
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-03-21 23:17:52 -0700

    Has Path BFS

---
+++ Structy/Graphs/HasPath_BFS.java
@@ -0,0 +1,25 @@
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayDeque;
+
+class Source {
+  public static boolean hasPath(Map<String, List<String>> graph, String src, String dst) {
+    ArrayDeque<String> queue = new ArrayDeque<>();
+    queue.add(src);
+    while(!queue.isEmpty()) {
+     String node = queue.remove();
+      if (node == dst) {
+        return true;
+      }
+      for (String neighbor : graph.get(node)) {
+        queue.add(neighbor);
+      }
+    }
+    return false;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
