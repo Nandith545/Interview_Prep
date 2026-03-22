da54ff3e724ec468f4466601c37f6e23133062c1
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-03-21 23:11:59 -0700

    Has Path Recursion

---
+++ Structy/Graphs/HasPath_Recursion.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+import java.util.List;
+
+class Source {
+  public static boolean hasPath(Map<String, List<String>> graph, String src, String dst) {
+    if (src == dst) {
+      return true;
+    }
+    for (String neighbor : graph.get(src)) {
+      if (hasPath(graph, neighbor, dst)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
