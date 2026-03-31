5a83ab40d5ab46dc563494b00ae9c746844f3ad4
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-03-30 23:55:12 -0700

    Adding time and space complexity

--- Structy/TwoPointer/UncompressString.java
+++ Structy/TwoPointer/UncompressString.java
@@ -24,3 +24,10 @@ class Source {
     // you may sandbox in this function, but should not remove it
   }
 }
+
+/*
+    n = number of groups
+m = max num found in any group
+Time: O(n*m)
+Space: O(n*m)
+*/
