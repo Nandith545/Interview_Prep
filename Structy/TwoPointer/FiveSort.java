13c92f1a1c7e59f1f0b4eb81135faa819d6b2878
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-01 13:47:01 -0700

    Five Sort at the end of the list in place

---
+++ Structy/TwoPointer/FiveSort.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.Collections;
+
+class Source {
+  public static List<Integer> fiveSort(List<Integer> array) {
+    int i = 0;
+    int j = array.size() - 1;
+
+    while(i < j){
+      if(array.get(j) == 5){
+        j -= 1;
+      }
+      else if(array.get(i) != 5){
+        i+=1;
+      }else{
+        Collections.swap(array, i, j);
+      }
+    }
+    return array;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function, but should not remove it
+  }
+}
