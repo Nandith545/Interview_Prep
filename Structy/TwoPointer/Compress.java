53c19ce1186b108d0a2e8f009a118ae2a91e4e54
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-01 13:40:26 -0700

    Compress String

---
+++ Structy/TwoPointer/Compress.java
@@ -0,0 +1,27 @@
+class Source {
+  public static String compress(String s) {
+    s += "!";
+    String result = "";
+    int i = 0;
+    int j = 0;
+    while (j < s.length()) {
+      if (s.charAt(i) == s.charAt(j)) {
+        j += 1;
+      } else {
+        int count = j - i;
+        if (count > 1) {
+          result += "" + count + s.charAt(i);
+        } else {
+          result += s.charAt(i);
+        }
+        i = j;
+      }
+    }
+    return result;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function, but should not remove it
+  }
+}
