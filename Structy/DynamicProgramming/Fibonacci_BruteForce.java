1a21c37137f78d84871cff486c56db064412796b
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-05 22:46:43 -0700

    Program for Fibonacci Brute force time out

---
+++ Structy/DynamicProgramming/Fibonacci_BruteForce.java
@@ -0,0 +1,13 @@
+class Source {
+  public static int fib(int n) {
+    if (n == 0 || n == 1) {
+      return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
