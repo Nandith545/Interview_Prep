1244f2b6108106623e37a248d19937efeae7295a
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-05 22:54:36 -0700

    Fibonacci series using DP

---
+++ Structy/DynamicProgramming/Fibonacci_DP.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+
+class Source {
+
+  public static int fib(int n) {
+    return fib(n, new HashMap<>());
+  }
+
+  
+  public static int fib(int n, HashMap<Integer, Integer> memo) {
+    //Base Case
+    if(n == 0 || n == 1){
+      return n;
+    }
+
+    if(memo.containsKey(n)){
+      return memo.get(n);
+    }
+    
+    int result = fib(n-1, memo) + fib(n-2, memo);
+    memo.put(n, result);
+    return result;
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
