876c5b1034aa2e8e21797b1397106238c0f0d7f1
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-09 22:35:31 -0500

    Count all the paths DP

---
+++ Structy/DynamicProgramming/CountPaths_DP.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.HashMap;
+
+class Source {
+  public static int countPaths(List<List<String>> grid) {
+    return countPaths(0, 0, grid, new HashMap<>());
+  }
+
+   public static int countPaths(int r, int c, List<List<String>> grid, HashMap<List<Integer>, Integer> memo) {
+
+     if( r == grid.size() || c == grid.get(0).size()){
+       return 0;
+     }
+
+     if(grid.get(r).get(c) == "X"){
+       return 0;
+     }
+
+     if(r == grid.size() - 1 && c == grid.get(0).size() - 1){
+       return 1;
+     }
+
+     List<Integer> pos = List.of(r,c);
+     if(memo.containsKey(pos)){
+       return memo.get(pos);
+     }
+     
+    int result = countPaths(r+1, c, grid, memo) + countPaths(r, c+1, grid, memo);
+    memo.put(pos, result);
+    return result;
+     
+  }
+
+  public static void run() {
+    // this function behaves as `main()` for the 'run' command
+    // you may sandbox in this function , but should not remove it
+  }
+}
