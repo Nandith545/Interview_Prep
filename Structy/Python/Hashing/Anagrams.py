b1e4e9b2e25b1a9b6c3498ed6bc77f2c84b1d1f9
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-12 17:19:40 -0500

    Adding time and space complexity

--- Structy/Python/Hashing/Anagrams.py
+++ Structy/Python/Hashing/Anagrams.py
@@ -9,3 +9,9 @@ def char_count(s):
     count[char] += 1
     
   return count
+
+# n is length of s1 and m is length of s2
+#Time Complexity: O(n + m)
+#Space Complexity: O(n + m) 
+#Reason: We are using a dictionary to store the character counts.
+#Reason: We are iterating through the strings to count the characters.  
