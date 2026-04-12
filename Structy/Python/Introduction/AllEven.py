3d8c51327a854c92b4bd09ad1d9bb0b8f9af328e
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-11 22:20:22 -0500

    Adding time and space complexity

--- Structy/Python/Introduction/AllEven.py
+++ Structy/Python/Introduction/AllEven.py
@@ -3,3 +3,8 @@ def all_even(nums):
     if num%2 == 1:
       return False
   return True
+
+  #Time Complexity: O(n)
+  #Reason: We are iterating through the list to check if all the numbers are even.
+  #Space Complexity: O(1)
+  #Reason: We are not using any extra space.
