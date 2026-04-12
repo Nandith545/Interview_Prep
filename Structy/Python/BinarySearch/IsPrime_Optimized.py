baf948df29e95e19030894822431666dbd39fd2e
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-11 22:40:14 -0500

    Adding time and space complexity

--- Structy/Python/BinarySearch/IsPrime_Optimized.py
+++ Structy/Python/BinarySearch/IsPrime_Optimized.py
@@ -8,3 +8,9 @@ def is_prime(n):
     if n % i == 0:
       return False
   return True
+
+  #Time Complexity: O(sqrt(n))
+  #Space Complexity: O(1)
+  #Reason: We are iterating through the range to check if the number is prime.
+  #Reason: We are not using any extra space.
+  #Reason: We are using the sqrt function to check if the number is prime.
