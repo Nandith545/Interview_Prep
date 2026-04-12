6f9a0535366530246f7e24881133f0cf7f61ff1a
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-12 17:25:26 -0500

    Adding time and space complexity

--- Structy/Python/Hashing/Anagrams_Counter_Library.py
+++ Structy/Python/Hashing/Anagrams_Counter_Library.py
@@ -3,3 +3,10 @@ from collections import Counter
 
 def anagrams(s1, s2):
   return Counter[Any](s1) == Counter[Any](s2)
+
+# n is length of s1 and m is length of s2
+#Time Complexity: O(n + m)
+#Space Complexity: O(n + m) 
+#Reason: We are using a dictionary to store the character counts.
+#Reason: We are iterating through the strings to count the characters.
+#Reason: We are comparing the character counts of the two strings.
