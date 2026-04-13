251e7a56383b553c6c5f172c40db51d52e75deae
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-12 21:29:35 -0500

    Adding time and space complexity

--- Structy/Python/Hashing/Intersection_Optimized.py
+++ Structy/Python/Hashing/Intersection_Optimized.py
@@ -3,3 +3,9 @@ from typing import Any
 def intersection(a, b):
   set_a = set[Any](a)
   return [item for item in b if item in set_a]
+
+# n is length of a and m is length of b
+#Time Complexity: O(n + m)
+#Space Complexity: O(n)
+#Reason: We are using a set to store the elements of a.
+#Reason: We are iterating through the elements of b to find the intersection.
