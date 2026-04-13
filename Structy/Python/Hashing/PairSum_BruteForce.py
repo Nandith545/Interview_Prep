cbc036f970607fcc188caec70754685d3f56f5af
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-12 20:24:23 -0500

    Adding time and space complexity

--- Structy/Python/Hashing/PairSum_BruteForce.py
+++ Structy/Python/Hashing/PairSum_BruteForce.py
@@ -4,3 +4,9 @@ def pair_sum(numbers, target_sum):
     for j in range(i+1, len(numbers)):
       if target_sum == numbers[i] + numbers[j]:
         return (i, j)
+
+# n is length of numbers
+#Time Complexity: O(n^2)
+#Space Complexity: O(1) 
+#Reason: We are using a nested loop to find the pair.
+#Reason: We are not using any extra space.
