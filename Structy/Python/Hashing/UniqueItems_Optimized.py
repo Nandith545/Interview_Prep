26e06778c80c70c989534904b0c09bf93413849d
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-12 21:54:10 -0500

    Adding time and space complexity

--- Structy/Python/Hashing/UniqueItems_Optimized.py
+++ Structy/Python/Hashing/UniqueItems_Optimized.py
@@ -1,3 +1,9 @@
 def all_unique(items):
   set_items = set(items)
   return len(items) == len(set_items)
+
+# n is length of items
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+#Reason: We are using a set to store the elements of items.
+#Reason: We are iterating through the elements of items to find the unique items.
