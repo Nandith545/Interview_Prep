59415dfb0cd7c5b3c4c15b038daff6360b58e2a8
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-13 13:41:42 -0500

    Adding time and space complexity

--- Structy/Python/Recursion/PalindromeRecursion.py
+++ Structy/Python/Recursion/PalindromeRecursion.py
@@ -5,3 +5,8 @@ def palindrome(s):
     return False
 
   return palindrome(s[1:-1])
+
+# n is the length of the string
+#Time Complexity: O(n^2)
+#Space Complexity: O(n^2)
+#Reason: We are using a recursive function to check if the string is a palindrome.
