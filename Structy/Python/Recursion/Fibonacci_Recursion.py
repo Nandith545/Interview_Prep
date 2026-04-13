8d656c4728023d355600251a0f4c9fc2a8593319
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-13 15:21:35 -0500

    Adding time and space complexity

--- Structy/Python/Recursion/Fibonacci_Recursion.py
+++ Structy/Python/Recursion/Fibonacci_Recursion.py
@@ -2,3 +2,8 @@ def fibonacci(n):
   if n < 2:
     return n
   return fibonacci(n - 1) + fibonacci(n - 2)
+
+# n is the number of times the function is called
+#Time Complexity: O(2^n)
+#Space Complexity: O(n)
+#Reason: We are using a recursive function to calculate the Fibonacci sequence.
