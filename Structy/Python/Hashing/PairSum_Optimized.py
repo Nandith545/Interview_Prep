5c9e63b700d2361057ae6363829eddf7722278bf
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-12 20:47:43 -0500

    Adding space and time complexity

--- Structy/Python/Hashing/PairSum_Optimized.py
+++ Structy/Python/Hashing/PairSum_Optimized.py
@@ -6,3 +6,9 @@ def pair_sum(numbers, target_sum):
     if complement in has_seen:
       return (index, has_seen[complement])
     has_seen[number] = index
+
+# n is length of numbers
+#Time Complexity: O(n)
+#Space Complexity: O(n) 
+#Reason: We are using a dictionary to store the numbers we have seen.
+#Reason: We are iterating through the numbers to find the pair.
