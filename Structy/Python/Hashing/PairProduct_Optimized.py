410b8f8a442e0022a668fe261e698a0a4f61b96f
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-12 21:01:28 -0500

    Adding time and space complexity

--- Structy/Python/Hashing/PairProduct_Optimized.py
+++ Structy/Python/Hashing/PairProduct_Optimized.py
@@ -6,3 +6,9 @@ def pair_product(numbers, target_product):
     if complement in has_seen:
       return (index, has_seen[complement])
     has_seen[num] = index
+
+# n is length of numbers    
+#Time Complexity: O(n)
+#Space Complexity: O(n) 
+#Reason: We are using a dictionary to store the numbers we have seen.
+#Reason: We are iterating through the numbers to find the pair.
