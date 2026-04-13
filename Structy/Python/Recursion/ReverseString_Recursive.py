91269b899b7b08a00f7a130a4c975c4c1e535b2f
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-13 13:25:36 -0500

    Adding time and space complexity

--- Structy/Python/Recursion/ReverseString_Recursive.py
+++ Structy/Python/Recursion/ReverseString_Recursive.py
@@ -2,3 +2,8 @@ def reverse_string(s):
   if len(s) == 0:
     return ""
   return reverse_string(s[1:]) + s[0]
+
+# n is the length of the string
+#Time Complexity: O(n^2)
+#Space Complexity: O(n^2)
+#Reason: We are using a recursive function to reverse the string.
