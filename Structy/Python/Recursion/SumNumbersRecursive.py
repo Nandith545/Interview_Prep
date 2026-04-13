cf7404f6f8501537fdbf7b26f716082b83791ce8
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-13 10:55:34 -0500

    Adding time and space complexity

--- Structy/Python/Recursion/SumNumbersRecursive.py
+++ Structy/Python/Recursion/SumNumbersRecursive.py
@@ -2,3 +2,8 @@ def sum_numbers_recursive(numbers):
   if len(numbers) == 0:
     return 0
   return numbers[0] + sum_numbers_recursive(numbers[1:])
+
+# n is Array length, n sub arrays are created using array slicing
+#Time Complexity: O(n^2)
+#Space Complexity: O(n^2)
+#Reason: We are using a recursive function to sum the numbers.
