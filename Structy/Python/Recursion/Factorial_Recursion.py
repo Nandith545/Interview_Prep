0120f97888656a9170c18d6392e199d50cd60ed7
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-13 11:03:24 -0500

    Adding time and space complexity

--- Structy/Python/Recursion/Factorial_Recursion.py
+++ Structy/Python/Recursion/Factorial_Recursion.py
@@ -2,3 +2,9 @@ def factorial(n):
   if n < 1:
     return 1
   return n * factorial(n - 1)
+
+
+# n is the number of times the function is called
+#Time Complexity: O(n)
+#Space Complexity: O(n)
+#Reason: We are using a recursive function to calculate the factorial.
