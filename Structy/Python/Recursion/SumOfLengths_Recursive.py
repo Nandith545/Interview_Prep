90aa80104f67f346dc4681a0c466cbc8ebb6784e
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-13 11:18:38 -0500

    Adding time and space complexity

--- Structy/Python/Recursion/SumOfLengths_Recursive.py
+++ Structy/Python/Recursion/SumOfLengths_Recursive.py
@@ -2,3 +2,8 @@ def sum_of_lengths(strings):
   if len(strings) == 0:
     return 0
   return len(strings[0]) + sum_of_lengths(strings[1:])
+
+# n is the number of times the function is called
+#Time Complexity: O(n^2)
+#Space Complexity: O(n^2)
+#Reason: We are using a recursive function to calculate the sum of lengths.
