ac76953426e3fefc6d00dfcc9aafd210631abcd5
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-14 13:07:58 -0500

    Diameter of a binary tree

---
+++ Structy/MetaTagged/DiameterOfBinaryTree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        self.res = 0
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        
+        #Returns height
+        def dfs(curr):
+            if not curr:
+                return 0
+            left = dfs(curr.left)
+            right = dfs(curr.right)
+            height = 1 + max(left,right)
+            self.res = max(self.res, left + right)
+            return height
+
+        dfs(root)
+        return self.res
