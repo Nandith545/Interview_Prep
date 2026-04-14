31151a0ca8bfd1f0a8b7527ef20f32c7e78e84c9
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-13 22:20:48 -0500

    Finding target in Linked List using recursion

---
+++ Structy/Python/LinkedList/LinkedListTarget_Recursive.py
@@ -0,0 +1,16 @@
+# class Node:
+#   def __init__(self, val):
+#     self.val = val
+#     self.next = None
+
+def linked_list_find(head, target):
+  if head is None:
+    return False
+  if head.val == target:
+    return True
+  return linked_list_find(head.next, target)
+  
+  # N-- No.of odes
+  # Time complexity - O(N)
+  # Space complexity - O(N)
+  
