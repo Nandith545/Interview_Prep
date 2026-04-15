dfe0ecccbbcfd529db9fc5bbdfd685c7001190ed
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-14 21:34:55 -0500

    Valid word abbreviation

---
+++ Structy/MetaTagged/ValidWordAbbreviation.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def validWordAbbreviation(self, word, abbr):
+        """
+        :type word: str
+        :type abbr: str
+        :rtype: bool
+        """
+        i, j = 0, 0
+
+        while i < len(word) and j < len(abbr):
+            if word[i] == abbr[j]:
+                i,j = i+1, j+1
+            elif abbr[j].isalpha() or abbr[j] == "0":
+                return False
+            else:
+                sublen = 0
+                while j < len(abbr) and not abbr[j].isalpha():
+                    sublen = sublen*10 + int(abbr[j])
+                    j += 1
+                i += sublen
+
+        return i == len(word) and j == len(abbr)
+
+
+        
