e0aa597c17d056462bfdaddb324852113cc049ee
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-16 16:13:51 -0500

    Complexity analysis added

--- Structy/MetaTagged/ArrayProductExceptSelf.py
+++ Structy/MetaTagged/ArrayProductExceptSelf.py
@@ -11,3 +11,7 @@ class Solution:
             res[i] *= postfix
             postfix *= nums[i]
         return res
+
+#Complexity Analysis
+#Time - O(2N) as we are doing 2 passes to find prefix and post fix products
+# Space - O(1) in the cotext of the problem as the output array is not considered as the extra space 
