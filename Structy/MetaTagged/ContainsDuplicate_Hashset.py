d372b9da24d7e579196cc5c7eedc1dce0b5c4bc6
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-17 14:07:24 -0500

    Complexity details added

--- Structy/MetaTagged/ContainsDuplicate_Hashset.py
+++ Structy/MetaTagged/ContainsDuplicate_Hashset.py
@@ -6,4 +6,8 @@ class Solution:
                 return True
             seen.add(n)
         return False
-        
+
+#Complexity analysis:
+#n - no. of elements in a list
+#Time - O(n) [worst case time complexity] - we traverse through each element until we find a duplicate
+#Space - O(n) using set to store seen values
