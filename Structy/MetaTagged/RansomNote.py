95a3527089150fb8da3b2f80f89d0ef2bed63315
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-17 18:49:32 -0500

    Can construct ransom note

---
+++ Structy/MetaTagged/RansomNote.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = {}
+
+        for c in magazine:
+            counter[c] = 1 + counter.get(c, 0)
+        for c in ransomNote:
+            if c not in counter:
+                return False
+            elif counter[c] == 1:
+                del counter[c]
+            else:
+                counter[c] -= 1
+        return True 
