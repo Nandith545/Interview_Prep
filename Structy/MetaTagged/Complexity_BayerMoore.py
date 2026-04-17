7ec95740148a3c74bb55a3c9cc8d3659972e5340
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-17 15:24:56 -0500

    Complexity details added

--- Structy/MetaTagged/Complexity_BayerMoore.py
+++ Structy/MetaTagged/Complexity_BayerMoore.py
@@ -6,6 +6,9 @@ class Solution:
         for n in nums:
             if count == 0:
                 cand = n
-            count += (1 if n -- cand else -1)
+            count += (1 if n == cand else -1)
         return cand
 
+#Complexity:
+#Time - O(n) - we have to traverse throuh all the elements in the list
+#Space - O(1) - We are not using any additional space 
