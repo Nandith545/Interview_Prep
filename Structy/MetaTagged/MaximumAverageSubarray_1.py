5604b01425569a94548e7fcea06331df28d19c6d
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-19 15:50:57 -0500

    Maximum Average Subarray

---
+++ Structy/MetaTagged/MaximumAverageSubarray_1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        if not nums:
+            return 0
+        cur_sum = max_sum = sum(nums[:k])
+        for i in range(len(nums) - k):
+            cur_sum += nums[i + k] - nums[i]
+            max_sum = max(max_sum, cur_sum)
+        return max_sum/k
