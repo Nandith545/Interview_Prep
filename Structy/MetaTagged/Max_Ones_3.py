8f61f6557b5dc3f5dad678ae31e9c3afea5bd768
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-19 16:46:41 -0500

    Max Consecutive Ones 3

---
+++ Structy/MetaTagged/Max_Ones_3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        start, end, max_ones = 0, 0, 0
+
+        while end < len(nums):
+            if nums[end] == 0:
+                k -= 1
+            while k < 0:
+                if nums[start] == 0:
+                    k +=1
+                start += 1
+            max_ones = max(max_ones, end - start + 1)
+            end += 1
+        return max_ones
+                    
+
+
+        
