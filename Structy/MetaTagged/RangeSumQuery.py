b14510f9e08d1e841a47385ae708eb77aec19db8
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-23 15:34:20 -0700

    Complexity Analysis

--- Structy/MetaTagged/RangeSumQuery.py
+++ Structy/MetaTagged/RangeSumQuery.py
@@ -18,3 +18,7 @@ class NumArray:
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
+
+#Complexity:
+#Time - O(N)
+#Space - O(1)
