b5bfebf7607092c548ba628564df0ad78c56a353
Nandith Reddy Malapati <nandith.malapati@gmail.com>
2026-04-23 22:22:04 -0700

    Find Peak element in an array

---
+++ Structy/MetaTagged/FindPeakElement.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0 , len(nums)- 1
+
+        while l <= r:
+            m = l + ((r - l)//2)
+            #Left neighbor is greater
+            if m > l and nums[m] < nums[m - 1]:
+                r = m - 1
+            #right neighbor is greater
+            elif m < r and nums[m] < nums[m + 1]:
+                l = m + 1
+            else:
+                return m
+
+
+        
